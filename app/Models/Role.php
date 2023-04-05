b2f5d5df0d10a03cb0d94dc6333813cdea65895c
Dan Bondarenko <dan.bondarenko.1@gmail.com>
2023-04-05 11:54:45 +0300

    refactor: add PhpDoc tags for magic properties of model classes

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,13 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property string $id
+ * @property Carbon $deleted_at
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property string $name
+ * @property Institution $institution
+ * @property Collection<PrivilegeRole> $privilegeRoles
+ * @property Collection<InstitutionUserRole> $institutionUserRoles
+ */
 class Role extends Model
 {
     use HasFactory, SoftDeletes, HasUuids;
@@ -18,7 +30,7 @@ class Role extends Model
         return $this->hasMany(InstitutionUserRole::class);
     }
 
-    public function rolePrivileges(): HasMany
+    public function privilegeRoles(): HasMany
     {
         return $this->hasMany(PrivilegeRole::class);
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property string $id
+ * @property Carbon $deleted_at
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property string $forename
+ * @property string $surname
+ * @property string $personal_identification_code
+ * @property Collection<InstitutionUser> $institutionUsers
+ */
 class User extends Authenticatable
 {
     use HasFactory, SoftDeletes, HasUuids;
