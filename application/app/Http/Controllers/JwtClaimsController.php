059b18709b9e0e44e39752427c737ce65c64eca8
Dan Bondarenko <dan.bondarenko.1@gmail.com>
2023-05-17 02:03:39 +0300

    feat(tv-sso#4): support returning partial claims when institution is not selected

--- application/app/Http/Controllers/JwtClaimsController.php
+++ application/app/Http/Controllers/JwtClaimsController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\GetJwtClaimsRequest;
 use App\Http\Resources\JwtClaims;
-use App\Services\JwtClaimsMaker;
+use App\Models\InstitutionUser;
+use App\Models\User;
 use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Support\Facades\Gate;
+use KeycloakAuthGuard\Services\Decoders\RequestBasedJwtTokenDecoder;
+use stdClass;
 
 class JwtClaimsController extends Controller
 {
-    public function __construct(private readonly JwtClaimsMaker $claimsMaker)
+    public function __construct(private readonly RequestBasedJwtTokenDecoder $jwtDecoder)
     {
     }
 
@@ -20,6 +24,46 @@ class JwtClaimsController extends Controller
      */
     public function show(GetJwtClaimsRequest $request): JwtClaims
     {
-        return $this->claimsMaker->make($request->validated());
+        Gate::allowIf($this->isAuthorized());
+
+        if ($request->has('institution_id')) {
+            $institutionUser = InstitutionUser::query()
+                ->whereRelation(
+                    'institution',
+                    'id',
+                    $request->validated('institution_id')
+                )
+                ->whereRelation(
+                    'user',
+                    'personal_identification_code',
+                    $request->validated('personal_identification_code')
+                )
+                ->firstOrFail();
+
+            return new JwtClaims($institutionUser);
+        }
+
+        $user = User::wherePersonalIdentificationCode($request->validated('personal_identification_code'))
+            ->firstOrFail();
+
+        return new JwtClaims($user);
+    }
+
+    private function isAuthorized(): bool
+    {
+        $decodedJwt = $this->jwtDecoder->getDecodedJwtWithSpecifiedValidation(false, false);
+
+        abort_if(empty($decodedJwt), 401);
+
+        return $this->isAuthorizedPartySsoInternalClient($decodedJwt);
+
+    }
+
+    private function isAuthorizedPartySsoInternalClient(stdClass $decodedJwt): bool
+    {
+        return property_exists($decodedJwt, 'azp')
+            && filled($tokenAuthorizedParty = $decodedJwt->azp)
+            && filled($expectedSsoInternalClientId = config('api.sso_internal_client_id'))
+            && $tokenAuthorizedParty === $expectedSsoInternalClientId;
     }
 }
--- application/app/Http/Resources/JwtClaims.php
+++ application/app/Http/Resources/JwtClaims.php
@@ -5,11 +5,13 @@ namespace App\Http\Resources;
 use App\Models\InstitutionUser;
 use App\Models\InstitutionUserRole;
 use App\Models\PrivilegeRole;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use UnexpectedValueException;
 
 /**
- * @mixin InstitutionUser
+ * @mixin InstitutionUser|User
  */
 class JwtClaims extends JsonResource
 {
@@ -22,22 +24,33 @@ class JwtClaims extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
-            'personalIdentificationCode' => $this->user->personal_identification_code,
-            'userId' => $this->user->id,
-            'institutionUserId' => $this->id,
-            'forename' => $this->user->forename,
-            'surname' => $this->user->surname,
-            'selectedInstitution' => [
-                'id' => $this->institution_id,
-                'name' => $this->institution->name,
+        return match (true) {
+            $this->resource instanceof InstitutionUser => [
+                'personalIdentificationCode' => $this->user->personal_identification_code,
+                'userId' => $this->user->id,
+                'institutionUserId' => $this->id,
+                'forename' => $this->user->forename,
+                'surname' => $this->user->surname,
+                'selectedInstitution' => [
+                    'id' => $this->institution_id,
+                    'name' => $this->institution->name,
+                ],
+                'privileges' => $this->institutionUserRoles
+                    ->flatMap(fn (InstitutionUserRole $iuRole) => $iuRole->role?->privilegeRoles)
+                    ->filter()
+                    ->map(fn (PrivilegeRole $privilegeRole) => $privilegeRole->privilege?->key)
+                    ->filter()
+                    ->unique(),
             ],
-            'privileges' => $this->institutionUserRoles
-                ->flatMap(fn (InstitutionUserRole $institutionUserRole) => $institutionUserRole->role?->privilegeRoles)
-                ->filter()
-                ->map(fn (PrivilegeRole $privilegeRole) => $privilegeRole->privilege?->key)
-                ->filter()
-                ->unique(),
-        ];
+            $this->resource instanceof User => [
+                'personalIdentificationCode' => $this->personal_identification_code,
+                'userId' => $this->id,
+                'forename' => $this->forename,
+                'surname' => $this->surname,
+            ],
+            default => throw new UnexpectedValueException(
+                'JwtClaims has unexpected resource type: '.get_class($this->resource)
+            )
+        };
     }
 }
