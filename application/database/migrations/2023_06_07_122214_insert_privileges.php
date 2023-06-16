5da3d7df277f26391487f5a40662f27984a56630
dmitriy <dmitriy@singleton.ee>
2023-06-16 10:25:36 +0300

    feat(184) - fix for insert tags privileges migration

--- application/database/migrations/2023_06_07_122214_insert_privileges.php
+++ application/database/migrations/2023_06_07_122214_insert_privileges.php
@@ -8,9 +8,9 @@ return new class extends InsertPrivilegesMigration
     public function getPrivilegesKeys(): array
     {
         return [
-            PrivilegeKey::AddTag,
-            PrivilegeKey::EditTag,
-            PrivilegeKey::DeleteTag
+            'ADD_TAG',
+            'EDIT_TAG',
+            'DELETE_TAG',
         ];
     }
 };
