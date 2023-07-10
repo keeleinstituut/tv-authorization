5445fe04b9bdc779cd03e360821978279e33c032
Dan Bondarenko <dan.bondarenko.1@gmail.com>
2023-07-10 16:51:38 +0300

    feat(#212): Define working time columns on institutions, update endpoints accordingly

--- application/app/Http/Resources/InstitutionResource.php
+++ application/app/Http/Resources/InstitutionResource.php
@@ -21,7 +21,22 @@ class InstitutionResource extends JsonResource
      *     short_name: string|null,
      *     logo_url: string|null,
      *     updated_at: Carbon,
-     *     created_at: Carbon
+     *     created_at: Carbon,
+     *     worktime_timezone: string|null,
+     *     monday_worktime_start: string|null,
+     *     monday_worktime_end: string|null,
+     *     tuesday_worktime_start: string|null,
+     *     tuesday_worktime_end: string|null,
+     *     wednesday_worktime_start: string|null,
+     *     wednesday_worktime_end: string|null,
+     *     thursday_worktime_start: string|null,
+     *     thursday_worktime_end: string|null,
+     *     friday_worktime_start: string|null,
+     *     friday_worktime_end: string|null,
+     *     saturday_worktime_start: string|null,
+     *     saturday_worktime_end: string|null,
+     *     sunday_worktime_start: string|null,
+     *     sunday_worktime_end: string|null
      * }
      */
     public function toArray(Request $request): array
@@ -35,6 +50,22 @@ class InstitutionResource extends JsonResource
             'logo_url',
             'created_at',
             'updated_at',
+            'worktime_timezone',
+
+            'monday_worktime_start',
+            'monday_worktime_end',
+            'tuesday_worktime_start',
+            'tuesday_worktime_end',
+            'wednesday_worktime_start',
+            'wednesday_worktime_end',
+            'thursday_worktime_start',
+            'thursday_worktime_end',
+            'friday_worktime_start',
+            'friday_worktime_end',
+            'saturday_worktime_start',
+            'saturday_worktime_end',
+            'sunday_worktime_start',
+            'sunday_worktime_end'
         );
     }
 }
