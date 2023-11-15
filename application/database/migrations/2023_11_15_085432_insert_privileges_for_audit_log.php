5faf57c40fc110337e177bf8e56a665665d71494
Markus <Markus>
2023-11-15 11:08:53 +0200

    update privilege keys

---
+++ application/database/migrations/2023_11_15_085432_insert_privileges_for_audit_log.php
@@ -0,0 +1,14 @@
+<?php
+
+use Database\Helpers\RootRoleAwareInsertPrivilegesMigration;
+
+return new class extends RootRoleAwareInsertPrivilegesMigration
+{
+    public function getPrivilegesKeys(): array
+    {
+        return [
+            'VIEW_AUDIT_LOG',
+            'EXPORT_AUDIT_LOG',
+        ];
+    }
+};
